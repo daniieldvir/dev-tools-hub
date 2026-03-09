42d454004ca007888ff491fecd1682644efca464
daniieldvir <daniieldvir2406@gmail.com>
2026-03-09 15:30:45 +0200

    UUID Generator

--- src/app/features/uuid-generator/uuid-generator.ts
+++ src/app/features/uuid-generator/uuid-generator.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Button } from '../../shared/components/button/button';
+import { LucideAngularModule } from 'lucide-angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-uuid-generator',
-  imports: [],
+  imports: [Button, LucideAngularModule, FormsModule],
   templateUrl: './uuid-generator.html',
   styleUrl: './uuid-generator.scss',
 })
-export class UuidGenerator {}
+export class UuidGenerator {
+  uuid = signal('');
+
+  generate() {
+    this.uuid.set(crypto.randomUUID());
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.uuid());
+  }
+
+  ngOnDestroy() {
+    this.uuid.set('');
+  }
+}
